2d3a43edaae07aa78d6caf4668d6f05f26447386
Mizuki Ohmiya <m7xtgrmd@gmail.com>
2020-09-04 20:18:48 +0900

    修正

---
+++ resources/views/feed/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Feed Add</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    {{--成功時のメッセージ--}}
+                    @if (session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                    @endif
+                    {{-- エラーメッセージ --}}
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                        </div>
+                    @endif
+
+                    <form action="{{ route('feed.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            </div>   
+                        </div>  
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary" name='action' value='add'>
+                                    {{ __('追加') }}
+                                </button>
+                                <button type="submit" class="btn btn-primary" name='action' value='back'>
+                                    {{ __('戻る') }}
+                                </button>
+                            </div>
+                        </div>
+                        <input type="hidden" class="form-control" name="cat_id" value="{{ $cat_id }}">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
